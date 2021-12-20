12038bced3b03854bd1fc11e3021d3e5a8472d93
ufukcanli <iletisim@ufukcanli.com>
2021-12-20 13:08:27 +0300

    Add initial screen

---
+++ CommunicationPatterns/CommunicationPatterns/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CommunicationPatterns
+//
+//  Created by Ufuk Canlı on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
